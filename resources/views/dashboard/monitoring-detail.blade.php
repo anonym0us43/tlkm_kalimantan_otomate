bbab8c637b88481f26af7cfab532011d9800ca88
Mahdian ID <mahdian043@gmail.com>
2025-12-12 15:50:57 +0800

    added btn

--- resources/views/dashboard/monitoring-detail.blade.php
+++ resources/views/dashboard/monitoring-detail.blade.php
@@ -879,7 +879,7 @@
 						<th>Site Detect</th>
 						<th>Site Name Detect</th>
 						<th>Tiket Terima</th>
-						<th>Aksi</th>
+						<th>Action</th>
 					</tr>
 				</thead>
 				<tbody id="detail-tbody">
@@ -963,12 +963,38 @@
 				<td>${row.site_name_detect || '-'}</td>
 				<td>${row.tiket_terima || '-'}</td>
 				<td>
-					<button type="button" class="btn btn-sm btn-primary planning-btn"
-						data-row-id="${row.row_id || ''}"
-						data-tt-site-id="${row.tt_site_id || ''}"
-						data-tt-site="${row.tt_site || '-'}">
-						Planning
-					</button>
+                    <div class="class="flex items-center">
+                        <button type="button" class="ltr:mr-2 rtl:ml-2 planning-btn"
+                            data-row-id="${row.row_id || ''}"
+                            data-tt-site-id="${row.tt_site_id || ''}"
+                            data-tt-site="${row.tt_site || '-'}">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16 4C18.175 4.01211 19.3529 4.10856 20.1213 4.87694C21 5.75562 21 7.16983 21 9.99826V15.9983C21 18.8267 21 20.2409 20.1213 21.1196C19.2426 21.9983 17.8284 21.9983 15 21.9983H9C6.17157 21.9983 4.75736 21.9983 3.87868 21.1196C3 20.2409 3 18.8267 3 15.9983V9.99826C3 7.16983 3 5.75562 3.87868 4.87694C4.64706 4.10856 5.82497 4.01211 8 4" stroke="#1C274C" stroke-width="1.5"/>
+                                <path d="M8 3.5C8 2.67157 8.67157 2 9.5 2H14.5C15.3284 2 16 2.67157 16 3.5V4.5C16 5.32843 15.3284 6 14.5 6H9.5C8.67157 6 8 5.32843 8 4.5V3.5Z" stroke="#1C274C" stroke-width="1.5"/>
+                                <path d="M15 13L12 13M12 13L9 13M12 13L12 10M12 13L12 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                            </svg>
+                        </button>
+                        <button type="button" class="ltr:mr-2 rtl:ml-2 approve-btn"
+                            data-row-id="${row.row_id || ''}"
+                            data-tt-site-id="${row.tt_site_id || ''}"
+                            data-tt-site="${row.tt_site || '-'}">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16 4C18.175 4.01211 19.3529 4.10856 20.1213 4.87694C21 5.75562 21 7.16983 21 9.99826V15.9983C21 18.8267 21 20.2409 20.1213 21.1196C19.2426 21.9983 17.8284 21.9983 15 21.9983H9C6.17157 21.9983 4.75736 21.9983 3.87868 21.1196C3 20.2409 3 18.8267 3 15.9983V9.99826C3 7.16983 3 5.75562 3.87868 4.87694C4.64706 4.10856 5.82497 4.01211 8 4" stroke="#1C274C" stroke-width="1.5"/>
+                                <path d="M9 13.4L10.7143 15L15 11" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M8 3.5C8 2.67157 8.67157 2 9.5 2H14.5C15.3284 2 16 2.67157 16 3.5V4.5C16 5.32843 15.3284 6 14.5 6H9.5C8.67157 6 8 5.32843 8 4.5V3.5Z" stroke="#1C274C" stroke-width="1.5"/>
+                            </svg>
+                        </button>
+                        <button type="button" class="ltr:mr-2 rtl:ml-2 reject-btn"
+                            data-row-id="${row.row_id || ''}"
+                            data-tt-site-id="${row.tt_site_id || ''}"
+                            data-tt-site="${row.tt_site || '-'}">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16 4C18.175 4.01211 19.3529 4.10856 20.1213 4.87694C21 5.75562 21 7.16983 21 9.99826V15.9983C21 18.8267 21 20.2409 20.1213 21.1196C19.2426 21.9983 17.8284 21.9983 15 21.9983H9C6.17157 21.9983 4.75736 21.9983 3.87868 21.1196C3 20.2409 3 18.8267 3 15.9983V9.99826C3 7.16983 3 5.75562 3.87868 4.87694C4.64706 4.10856 5.82497 4.01211 8 4" stroke="#1C274C" stroke-width="1.5"/>
+                                <path d="M8 3.5C8 2.67157 8.67157 2 9.5 2H14.5C15.3284 2 16 2.67157 16 3.5V4.5C16 5.32843 15.3284 6 14.5 6H9.5C8.67157 6 8 5.32843 8 4.5V3.5Z" stroke="#1C274C" stroke-width="1.5"/>
+                                <path d="M14.5 11L9.50004 16M9.50002 11L14.5 16" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                            </svg>
+                        </button>
+                    </div>
 				</td>
 			`;
 				tbody.appendChild(tr);
@@ -1139,7 +1165,7 @@
 											<div class="photos-grid" data-row-id="${rowId}">
 												<div class="photos-empty">Loading photos...</div>
 											</div>
-										</div>
+										</div>x
 									</div>
 									<div class="form-section">
 									<div class="mb-6">
