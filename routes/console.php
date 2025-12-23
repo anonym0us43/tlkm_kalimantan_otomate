33c698e67123eda5efddffc0a9bf7895d493d4ed
Mahdian ID <mahdian043@gmail.com>
2025-12-23 22:00:47 +0800

    added

--- routes/console.php
+++ routes/console.php
@@ -1,9 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function ()
+Artisan::command('telegram:webhook', function ()
 {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+    \App\Http\Controllers\TelegramController::webhookOtomateBot();
+});
